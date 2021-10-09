27e1d81e0e528f257df49574cf5b0101c0db1bab
Hideto Ueno <uenoku.tokotoko@gmail.com>
2021-10-09 15:11:22 +0900

    tmp

--- const.ml
+++ const.ml
@@ -442,7 +442,7 @@ let type_of = function
   | Nil(ty) -> Type.mk_list ty
   | Cons(ty) -> Type.mk_fun [ty; Type.mk_list ty; Type.mk_list ty]
   | Hd(ty) -> Type.mk_fun [Type.mk_list ty; ty] 
-  | Tl(ty) -> Type.mk_fun [Type.mk_list ty; Type.mk_list ty] 
+  | Tl(ty) -> Type.mk_fun [ty; ty] 
   | IsNil(ty) -> Type.mk_fun [Type.mk_list ty; Type.mk_bool] 
   (* uninterpreted functions *)
   | UFun(ty, _) -> ty
